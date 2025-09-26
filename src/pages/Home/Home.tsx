a7abe1a52f5b1dd40b25e3437314560459880ad8
Arthur Pimentel <100161957+artpimentel@users.noreply.github.com>
2025-09-26 11:13:34 -0300

    HomePage Implementation

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,36 @@
+import styles from "./Home.module.css";
+
+import Slider from "../../components/Slider";
+
+import Catalog from "../../components/Catalog";
+import type { Producer } from "../../types/Producer.ts";
+import allProducers from "../../data/producers.ts";
+
+function Home2() {
+  const producersNearYou = allProducers.filter(
+    (producer: Producer) => producer.locality === "Rio de Janeiro"
+  );
+
+  const producersYouLike = allProducers.filter((producer: Producer) =>
+    producer.tags.includes("loira")
+  );
+
+  return (
+    <>
+      <Slider />
+
+      <Catalog
+        producers={producersYouLike}
+        title="Produtoras para o seu Gosto"
+        filter="Loira"
+      />
+      <Catalog
+        producers={producersNearYou}
+        title="Produtoras Próximas de Você"
+        filter="Rio de Janeiro"
+      />
+    </>
+  );
+}
+
+export default Home2;
