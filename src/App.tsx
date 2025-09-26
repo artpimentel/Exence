319e7689fd17b8c0350915fadccbdcc19712d7ec
Arthur Pimentel <100161957+artpimentel@users.noreply.github.com>
2025-09-26 10:57:25 -0300

    Routes Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./styles/App.css";
 import MainLayout from "./layout/MainLayout";
+import HomePage from "./pages/Home";
+import RegisterPage from "./pages/Register";
 
 function App() {
-  return <MainLayout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <HomePage />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <MainLayout>
+              <RegisterPage />
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
