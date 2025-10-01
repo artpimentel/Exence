93506009182d0a1688e03fbdd3cbd2457d66a336
Arthur Pimentel <100161957+artpimentel@users.noreply.github.com>
2025-10-01 15:30:12 -0300

    ScrollTop Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 import "./styles/App.css";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  }, [pathname]);
-
-  return null;
-}
+import { ScrollTop } from "./utils/ScrollTop";
 
 function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
+      <ScrollTop />
       <AppRoutes />
     </BrowserRouter>
   );
