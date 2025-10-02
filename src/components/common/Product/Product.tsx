8b4313bb45528c8b524db14ecb032b6f150982d4
Arthur Pimentel <100161957+artpimentel@users.noreply.github.com>
2025-10-02 20:37:52 -0300

    Product Update (Initial)

--- src/components/common/Product/Product.tsx
+++ src/components/common/Product/Product.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 import styles from "./Product.module.css";
+import { GrLocation } from "react-icons/gr";
+import { BiIdCard } from "react-icons/bi";
+
 import type { Producer } from "../../../types/Producer";
 
 interface ProductProps {
@@ -15,11 +18,43 @@ function Product({ producer, variant }: ProductProps) {
         ${variant === "highlight" ? styles.highlight : ""} 
         ${variant === "row" ? styles.row : ""}`}
     >
-      <Link to={`/product/${producer.id}`}>
+      <Link to={`/product/${producer.id}`} className={styles.productLink}>
         <img className={styles.producerImage} src={producer.images[0]} alt="" />
+
         <div className={styles.itemInfo}>
-          <h3 className={styles.producerName}>{producer.name}</h3>
-          <span className={styles.procuderLocality}>{producer.locality}</span>
+          <div className={styles.infoHeader}>
+            <h3 className={styles.producerName}>{producer.name}</h3>
+            <span
+              className={`${styles.productLoc} 
+                ${variant === undefined ? styles.hidden : ""}`}
+            >
+              {producer.locality}
+            </span>
+            <span
+              className={`${styles.productDesc} 
+                ${variant !== undefined ? styles.hidden : ""}`}
+            >
+              {producer.description}
+            </span>
+          </div>
+          <div
+            className={`${styles.infoContent} 
+                ${variant !== undefined ? styles.hidden : ""}`}
+          >
+            <div className={styles.infoValue}></div>
+            <span className={styles.defaultInfo}>
+              <span>
+                <BiIdCard />
+              </span>
+              {producer.age} anos
+            </span>
+            <span className={styles.defaultInfo}>
+              <span>
+                <GrLocation />
+              </span>
+              {producer.locality}
+            </span>
+          </div>
         </div>
       </Link>
     </li>
