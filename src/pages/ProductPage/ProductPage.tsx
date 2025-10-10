b8733c31395de3a5f26ca0c09a021018adde3d78
artpimentel <art.ol.pimentel@gmail.com>
2025-10-10 09:47:07 -0300

    BuildFix

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -7,29 +7,7 @@ import type { Producer } from "../../types/Producer";
 
 import Slider from "../../components/Slider/Slider";
 import ProductInfo from "../../components/ProductCard/ProductInfo";
-
-import { FaCheck, FaXmark } from "react-icons/fa6";
-import { IoIosArrowDown } from "react-icons/io";
-
-const formatServiceName = (name: string): string => {
-  const serviceMap: { [key: string]: string } = {
-    Companion: "Acompanhante",
-    Trip: "Viagem",
-    Kiss: "Beijo",
-    Masturbation: "Masturbação",
-    OralSex: "Sexo Oral",
-    AnalSex: "Sexo Anal",
-    VaginalSex: "Sexo Vaginal",
-    OralSexWithCondom: "Sexo Oral com Preservativo",
-    AnalSexWithCondom: "Sexo Anal com Preservativo",
-    VaginalSexWithCondom: "Sexo Vaginal com Preservativo",
-    DoublePenetration: "Penetração Dupla",
-    TriplePenetration: "Penetração Tripla",
-    Squirt: "Squirt",
-  };
-
-  return serviceMap[name] || name;
-};
+import ProductServices from "../../components/ProductServices/ProductServices";
 
 function ProductPage() {
   const { id } = useParams<{ id: string }>();
@@ -47,25 +25,6 @@ function ProductPage() {
     alt: `${producer.profile.name} - imagem ${index + 1}`,
   }));
 
-  const servicesEntries = Object.entries(producer.services) as [
-    keyof Producer["services"],
-    boolean
-  ][];
-
-  const offeredServices = servicesEntries
-    .filter(
-      ([key, value]) =>
-        key !== "fetishes" && typeof value === "boolean" && value === true
-    )
-    .map(([key]) => formatServiceName(key));
-
-  const notOfferedServices = servicesEntries
-    .filter(
-      ([key, value]) =>
-        key !== "fetishes" && typeof value === "boolean" && value === false
-    )
-    .map(([key]) => formatServiceName(key));
-
   return (
     <div className={styles.productPage}>
       <section className={styles.productShowcase}>
@@ -74,35 +33,7 @@ function ProductPage() {
           <ProductInfo producer={producer} />
         </div>
       </section>
-      <section className={styles.productServices}>
-        <div className={`${styles.column} ${styles.servicesY}`}>
-          <h2>Faço</h2>
-          <ul className={styles.servicesList}>
-            {offeredServices.map((serviceName) => (
-              <li key={serviceName}>
-                <span className={styles.green}>
-                  <FaCheck />
-                </span>
-                {serviceName}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className={`${styles.column} ${styles.servicesN}`}>
-          <h2>Não faço</h2>
-          <ul className={styles.servicesList}>
-            {notOfferedServices.map((serviceName) => (
-              <li key={serviceName}>
-                <span className={styles.red}>
-                  <FaXmark />
-                </span>
-                {serviceName}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
+      <ProductServices producer={producer} />
     </div>
   );
 }
