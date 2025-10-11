ca560150cd4d6ca451bf721900645a2da63096ba
artpimentel <art.ol.pimentel@gmail.com>
2025-10-11 11:10:14 -0300

    StartPopup Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 import "./styles/App.css";
-
+import StartPopup from "./components/StartPopup/StartPopup";
 import { ScrollTop } from "./utils/ScrollTop";
 
 function App() {
+  const [showPopup, setShowPopup] = useState(false);
+  const [hasConfirmedAge, setHasConfirmedAge] = useState(false);
+
+  useEffect(() => {
+    const ageConfirmed = sessionStorage.getItem("ageConfirmed");
+
+    if (ageConfirmed === "true") {
+      setHasConfirmedAge(true);
+    } else {
+      setShowPopup(true);
+    }
+  }, []);
+
+  const handleConfirmAge = () => {
+    setShowPopup(false);
+    setHasConfirmedAge(true);
+  };
+
+  const handleExitSite = () => {
+    setShowPopup(false);
+    window.location.href = "https://www.google.com";
+  };
+
+  if (!hasConfirmedAge) {
+    return (
+      <StartPopup onConfirmAge={handleConfirmAge} onExitSite={handleExitSite} />
+    );
+  }
+
   return (
     <BrowserRouter>
       <ScrollTop />
