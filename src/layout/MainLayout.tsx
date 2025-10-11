39a5b88b9a5d9173256a81bb9f8740143e84fc36
artpimentel <art.ol.pimentel@gmail.com>
2025-10-11 10:47:26 -0300

    Footer Implementation

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import type React from "react";
 
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
