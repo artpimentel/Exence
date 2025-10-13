3da01f65ca4c2f0fc71df6ad808ec2e57c03921a
artpimentel <art.ol.pimentel@gmail.com>
2025-10-13 09:47:21 -0300

    FontUpdate

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
+import "./styles/Fonts.css";
+import "./styles/variables.css";
+
 import App from "./App.tsx";
 import "./i18n";
 
